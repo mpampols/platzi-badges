2ccb582a2dcc3bee42fd051fda6dceccd7c4c3af
Marc Pàmpols <mpampols@gmail.com>
2020-03-10 00:41:19 +0100

    Updates

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,34 @@
 import React from 'react';
 
+import './styles/Badge.css'
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
     render() {
-        return <div>
-            <div>
-                <img src={confLogo} alt="Logo de la conferencia" />
+        return (
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={confLogo} alt="Logo de la conferencia" />
+                </div>
+                
+                <div className="Badge__section-name">
+                    <img 
+                        className="Badge__avatar"
+                        src="{this.props.avatarUrl}" 
+                        alt="Avatar" />
+                    <h1>{this.props.firstName} <br/> {this.props.lastName}</h1>
+                </div>
+                
+                <div className="Badge__section-info">
+                    <h3>{this.props.jobTitle}</h3>
+                    <div>@{this.props.twitter}</div>
+                </div>
+
+                <div className="Badge__footer">
+                    #mpampols
+                </div>
             </div>
-            
-            <div>
-                <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
-                <h1>Richard <br/> Kaufman</h1>
-            </div>
-            
-            <div>
-                <p>Frontend Engineer</p>
-                <p>@sparragus</p>
-            </div>
-        </div>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import './global.css';
 import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(<Badge 
+                    firstName="Eli" 
+                    lastName="Martin" 
+                    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
+                    jobTitle="Engineer" 
+                    twitter="mpampols"/>, container);
